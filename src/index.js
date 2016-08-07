8a7d6194cc5d57d327dfcf2d543abd6035894668
Chris Malven <cmalven@chrismalven.com>
2016-08-07 14:01:33 -0500

    Adds basic implementation of loop points with persistance between sessions.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ var styles = require('css!./main.css');
 var sliderStyles = require('css!./no-ui-slider.css');
 var markup = require("html!./index.html");
 var noUiSlider = require('noUiSlider');
+var localforage = require('localforage')
 
 module.exports = function(timeline, options) {
       /*
@@ -27,7 +28,12 @@ module.exports = function(timeline, options) {
         activeTimescaleClass: 'is-active',
         labelsSelector: '.js-anim-panel-labels',
         timeSelector: '.js-time',
-        shouldUpdateSliderFromTimeline: true
+        loopInSelector: '.js-loop-in',
+        loopOutSelector: '.js-loop-out',
+        loopClearSelector: '.js-loop-clear',
+        shouldUpdateSliderFromTimeline: true,
+        loopIn: null,
+        loopOut: null
       };
      
      
@@ -58,6 +64,7 @@ module.exports = function(timeline, options) {
         _addStyles();
         _addSlider();
         _addLabelButtons();
+        _setLoop();
         _addEventListeners();
         _play();
       };
@@ -148,15 +155,41 @@ module.exports = function(timeline, options) {
           })
         });
       };
+
+      var _setLoop = function() {
+        // Set default values
+        _setLoopDefaults();
+
+        // Check for local storage values
+        localforage.getItem('loopIn', function(err, val) {
+          if (val) {
+            self.loopIn = val;
+            timeline.time(self.loopIn);
+          }
+        });
+        localforage.getItem('loopOut', function(err, val) {
+          if (val) {
+            self.loopOut = val;
+          }
+        });
+      };
      
       var _addEventListeners = function() {
+        // Playback Controls
         document.querySelector(self.playSelector).addEventListener('click', _play.bind(self));
         document.querySelector(self.pauseSelector).addEventListener('click', _pause.bind(self));
         document.querySelector(self.restartSelector).addEventListener('click', _restart.bind(self));
+        
+        // Setting Timescale
         document.querySelectorAll(self.timescaleSelector).forEach(function(timescaleLink) {
           var timescale = timescaleLink.getAttribute('data-timescale');
           timescaleLink.addEventListener('click', _updateTimescale.bind(self, timescaleLink, timescale));
         });
+
+        // Setting Loop
+        document.querySelector(self.loopInSelector).addEventListener('click', _setLoopIn.bind(self));
+        document.querySelector(self.loopOutSelector).addEventListener('click', _setLoopOut.bind(self));
+        document.querySelector(self.loopClearSelector).addEventListener('click', _clearLoop.bind(self));
 
         // Listen for the playhead to change
         timeline.eventCallback('onUpdate', _onTimelineUpdate.bind(self))
@@ -174,13 +207,52 @@ module.exports = function(timeline, options) {
 
       var _restart = function(evt) {
         timeline.timeScale(1);
-        timeline.restart();
+        timeline.time(self.loopIn);
         document.querySelector(self.pauseSelector).classList.remove(self.activeTimescaleClass);
+      };
+
+      var _setLoopIn = function(evt) {
+        var currentTime = timeline.totalTime();
+
+        // Prevent setting an in time after the out time
+        if (currentTime > self.loopOut) return;
+
+        localforage.setItem('loopIn', currentTime, function(err, val) {});
+        self.loopIn = currentTime;
+        console.log('Loop In Set: ', currentTime);
+      };
+
+      var _setLoopOut = function(evt) {
+        var currentTime = timeline.totalTime();
+
+        // Prevent setting an out time before the in time
+        if (currentTime < self.loopIn) return;
+
+        localforage.setItem('loopOut', currentTime, function(err, val) {});
+        self.loopOut = currentTime;
+        console.log('Loop Out Set: ', currentTime);
+      };
+
+      var _clearLoop = function(evt) {
+        localforage.setItem('loopIn', null, function(err, val) {});
+        localforage.setItem('loopOut', null, function(err, val) {});
+        _setLoopDefaults();
+      };
+
+      var _setLoopDefaults = function(evt) {
+        self.loopIn = 0;
+        self.loopOut = timeline.totalDuration();
+        console.log('Loop Reset: In ' + self.loopIn + ', Out ' + self.loopOut);
       };
 
       var _onTimelineUpdate = function() {
         // Update the displayed time
         _updateTime(timeline.totalTime());
+
+        // If we're at the loop out point, jump to the loop in point
+        if (timeline.totalTime() >= self.loopOut) {
+          timeline.time(self.loopIn);
+        }
 
         // Update slider based on timeline
         if (!self.shouldUpdateSliderFromTimeline) return;
@@ -208,6 +280,8 @@ module.exports = function(timeline, options) {
       //   Public Methods
       //
       //////////////////////////////////////////////////////////////////////
+      
+      /* No public methods yet, but if you add one it should use the following signature: */
       
       /*
       self.foo = function() {
